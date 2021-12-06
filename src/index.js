18ae2c6f9eea75bbb44a02c676da6d1236516a89
Mateus Silva Seixas <mateusinhokarambi@hotmail.com>
2021-12-06 20:52:43 -0300

    Aprendendo sobre query params, route params e body

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 01',
     'Projeto 02'
@@ -10,6 +17,11 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 01',
     'Projeto 02',
@@ -18,6 +30,14 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
+  console.log(id);
+
   return response.json([
     'Projeto 01',
     'Projeto 02',
